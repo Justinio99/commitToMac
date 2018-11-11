57ae5574ee34b44bdeb7101fb1d656922d18eee2
Justin Spring <justinspring@justins-imac.home>
2018-11-11 15:08:16 +0100

    added AuthReducer and action

--- ChatApp/App.js
+++ ChatApp/App.js
@@ -1,19 +1,21 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import { Router, Scene } from 'react-native-router-flux';
-import Dashboard from './components/dashboard/Dashboard'
-import Home from './components/Auth/Home'
+import store from './src/store'
+import { Provider } from 'react-redux'
+import Dashboard from './src/components/dashboard/Dashboard'
+import Home from './src/components/Auth/Home'
 export default class App extends Component {
   render() {
     return (
-      
+      <Provider store={store}>
       <Router>
         <Scene key="root" >
           <Scene key="home" component={Home} />
           <Scene key="tab1" component={Dashboard} />
         </Scene>
       </Router>
-
+      </Provider>
     );
   }
 }
