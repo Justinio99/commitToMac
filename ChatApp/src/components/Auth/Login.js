0cd69d12f15c01024756200b0e60d2f102824507
Justin Spring <justinspring@justins-imac.home>
2018-11-12 22:16:55 +0100

    work

--- ChatApp/src/components/Auth/Login.js
+++ ChatApp/src/components/Auth/Login.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux'
 import React, { Component } from 'react'
 import { signIn } from '../../store/actions/AuthAction'
-import {Platform, StyleSheet, Text, View, TextInput} from 'react-native';
+import {Button, StyleSheet, Text, View, TextInput} from 'react-native';
 export class Login extends Component {
   state = {
     email: '',
@@ -18,15 +18,20 @@ export class Login extends Component {
    e.preventDefault();
    this.props.signIn(this.state);
     this.props.history.push('/');
-
-   
   }
+  login(){
+    this.props.signIn(this.state)
+  }
+  
 
 render() {
   return (
     <View>
-    <Text>email</Text>
-    <TextInput onChangeText={(text)=> this.state.email = text}></TextInput>
+      <Text>email</Text>
+      <TextInput onChangeText={(email)=> this.state.email = email} style={styles.textInput}></TextInput>
+      <Text>Password</Text>
+      <TextInput onChangeText={(pass)=> this.state.password = pass} style={styles.textInput}></TextInput>
+      <Button onPress={()=> this.login()} title={'Login'}></Button>
     </View>
   )
 }
@@ -38,8 +43,19 @@ const mapStateToProps = (state) => {
 }
 const mapDispachToProps = (dispatch) =>{
   return{
-   
+    signIn: (credentials) => dispatch(signIn(credentials))
   }
 }
 
 export default connect(mapStateToProps, mapDispachToProps)(Login)
+
+const styles = StyleSheet.create({
+  textInput:{
+    borderStyle: 'solid',
+    borderColor: 'black',
+    borderWidth: 0.5,
+    height: 40,
+    width:250,
+  },
+});
+
